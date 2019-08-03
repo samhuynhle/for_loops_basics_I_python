1f8dcaf51e4340eb813c98cd1b0dd72939d6e8fe
Sam H Le <samhuynhle@gmail.com>
2019-08-02 21:39:05 -0700

    finished assignment

--- for_loops_basic_I.py
+++ for_loops_basic_I.py
@@ -21,12 +21,30 @@
 #     else:
 #         print(x)
 #     x += 1
-
 # Zero is both divisiable by 10 and 5.
 
 # Whoa. That Sucker's Huge - Add odd integers from 0 to 500,000, and print the final sum.
 
+# sum = 0
+# for whoa in range (0, 500001, 1):
+#     if (whoa % 2) == 1:
+#         sum = sum + whoa
+# print(sum)
+
 # Countdown by Fours - Print positive numbers starting at 2018, counting down by fours.
+
+# for x in range (2018, 0, -4):
+#     print(x)
 
 # Flexible Counter - Set three variables: lowNum, highNum, mult. Starting at lowNum and going through highNum, print only the integers that are a multiple of mult.
 # For example, if lowNum=2, highNum=9, and mult=3, the loop should print 3, 6, 9 (on successive lines)
+
+# lowNum = 1
+# highNum = 53
+# mult = 7
+
+# for x in range (lowNum, highNum + 1, 1):
+#     if (x % mult) == 0:
+#         print(x)
+
+
